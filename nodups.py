9062efaff2cf1addc0bf91c51d3b58d9c4878369
Richard Magahiz <frabdango@magahiz.com>
2014-08-02 15:42:06 -0400

    nodups.py is a class

--- nodups.py
+++ nodups.py
@@ -1,4 +1,5 @@
-def remove_dupes(string): 
+class Nodups
+ def remove_dupes(string): 
    out = [] 
    seen = {} 
    for i in range(0, len(string)): 
@@ -7,6 +8,5 @@ def remove_dupes(string):
        seen[string[i]] = True 
        out.append(string[i]) 
    return "".join(out)
-
-aa = "When in the course of human events"
-print remove_dupes(aa)
+ end
+end
