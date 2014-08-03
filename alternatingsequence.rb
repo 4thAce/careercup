7fb4fe99a30f78ef1a1748aeb4b10e962b00b95e
Richard Magahiz <frabdango@magahiz.com>
2014-08-02 21:51:53 -0400

    Get the negatives in also

--- alternatingsequence.rb
+++ alternatingsequence.rb
@@ -1,58 +1,51 @@
 # http://www.careercup.com/question?id=5714091033231360
 #
-# nil nil -3 -7 4 8 -12 -5 4 14
-# 4  nil -3 -7  8 -12 -5 4 14
-# 4 -3  -7  8 -12 -5 4 14
-# Now the array is back to its original size. Pad it after the first
-#  two entries, repeat the operation
-# 4 -3 nil nil -7 8 -12 -5 4 14
-# 4 -3 8   nil -7 -12 -5 4 14
-# 4 -3 8  -7 -12 -5 4 14
-# Then keep padding and searching until you exhaust either positives
-# or negatives. Eliminate any remaining nils at the end. This adds
-# only space complexity O(1) and the time complexity will be something
-# like O(N^2). The respective positives and negatives will keep
-# their orders
 
 class AlternatingSequence
   def alternatingsequence(input)
-    puts "Array is " + input.join(',')
     return input if input.all? { |entry| entry > 0 }
     return input if input.all? { |entry| entry < 0 }
 # Pad the array with two initial values set to nil
-#    working = [nil, nil] + input
     working = input
     length = working.length
     pointer = 0
-    catch Exception do
-       while pointer < length do
+    while pointer < length - 1 do
 # The first nil is filled with the positive entry with lowest
 #  index greater than pointer
-         working[pointer..-1].each_with_index do |value, index|
-           puts "%s at %s" % [value,index]
-           if value > 0
-             puts "Replacing value at #{pointer}"
-             if pointer > 0
-             puts "#{working[0..pointer-1]} + #{[working[index]]} + #{working[pointer..index-1]} + #{working[index+1..-1]}"
-               working = working[0..pointer-1] + [working[index]] + working[pointer..index-1] + working[index+1..-1]
-             else
-             puts "#{[working[index]]} + #{working[pointer..index-1]} + #{working[index+1..-1]}"
-               working = [working[index]] + working[pointer..index-1] + working[index+1..-1]
-             end
-             throw Exception
-           else
-             puts "#{value} < 0"
-           end
-         end
+      if working[pointer] < 0
+        working[pointer..-1].each_with_index do |value, index|
+          if value > 0
+            posindex = pointer + index
+            if pointer > 0
+              working = working[0..pointer-1] + [working[posindex]] + working[pointer..posindex-1] + working[posindex+1..-1]
+            else
+              working = [working[posindex]] + working[pointer..posindex-1] + working[posindex+1..-1]
+            end
+# Break out of the loop for the positive entry
+            break
+          end
 # There are no more positive entries so we are done
-# Delete that entry
-# The second nil is filled with the negative entry with lowest index
-# Delete that entry
-
-# Increment pointer by two, put the two padding entries there
-    pointer = pointer + 2
-       end
-    end
+        end    # loop over tail of working, break to here
+        #return working
+      end  #if
+      return working if length < pointer+1 
+# Coerce the second spot with the negative entry with lowest index
+      if working[pointer + 1] > 0
+        working[pointer+1..-1].each_with_index do |value, index|
+          if value < 0
+            negindex = pointer + index + 1
+            working = working[0..pointer] + [working[negindex]] + working[pointer+1..negindex-1] + working[negindex+1..-1]
+            break
+          else
+            working
+          end
+# There are no more positive entries so we are done
+        end    # loop over tail of working
+      else
+        #return working
+      end # if
+      pointer = pointer + 2
+    end #while loop over pointer
     working
-  end
+  end #def
 end
--- spec/alternatingsequence_spec.rb
+++ spec/alternatingsequence_spec.rb
@@ -27,7 +27,28 @@ RSpec.describe AlternatingSequence do
     a = AlternatingSequence.new
     result = a.alternatingsequence(input)
     expect(result).to eq([1, -1, -2])
-  end
+  end 
+
+  it "returns [2, -1, -10] if given [-1, 2, -10]" do
+    input = [-1, 2, -10]
+    a = AlternatingSequence.new
+    result = a.alternatingsequence(input)
+    expect(result).to eq([2, -1, -10])
+  end 
+
+  it "returns [1, -10, 2] if given [1, 2, -10]" do
+    input = [1, 2, -10]
+    a = AlternatingSequence.new
+    result = a.alternatingsequence(input)
+    expect(result).to eq([1, -10, 2])
+  end 
+
+  it "returns [4, -3, 8,  -7, 4, -12, 14, -5] given [-3 -7 4 8 -12 -5 4 14]" do
+    input = [-3, -7, 4, 8, -12, -5, 4, 14]
+    a = AlternatingSequence.new
+    result = a.alternatingsequence(input)
+    expect(result).to eq([4, -3, 8,  -7, 4, -12, 14, -5])
+  end 
 
 # it "returns [1, -1, 2, -2] if given [-1, 1, -2, 2]" do
 #   input = [-1, 1, -2, 2]
@@ -35,8 +56,6 @@ RSpec.describe AlternatingSequence do
 #   result = a.alternatingsequence(input)
 #   expect(result).to eq([1, -1, 2, -2])
 # end
-
-
 
 #it "returns the original array if already alternating" do
 #  input = [1, -2, 24, -2, 1]
